922c10773a7ad498c7d544e42bdfb66e16146aad
YuliaCheprasova <cheprasovaja@yandex.ru>
2023-03-03 10:48:39 +0700

    documentation

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import datetime
-class person():
-    def wish(self):
+class wish():
+    """The class that generates wish of a person through dialogue"""
+    def subject(self) -> str:
+        """The method that determines a subject of sending"""
         print('I want to send (write who or what) ')
         who = input()
         return who
-    def place(self, who):
+    def place(self, who: str) -> str:
+        """The method that determines a place of sending"""
         print('I want to send a ', who, ' to (write anywhere in the world) ')
         where = input()
         return where
-    def time(self, who, where):
+    def time(self, who: str, where: str) -> int:
+        """The method that determines the date of sending
+        and calculates the difference between it and the present time"""
         while (1):
             print(
-                'I want to send a ', who,' to ', where, ' in (write the date you want in the year-month-date format, for example, 2028-06-09) ')
+                'I want to send a ', who,' to ', where, ' in (write the date you want in the '
+                                                        'year-month-date format, for example, 2028-06-09) ')
             when = input()
             when = datetime.datetime.strptime(when, '%Y-%m-%d')
             when = when.date()
@@ -21,21 +27,27 @@ class person():
             tdelta = tdelta.days
             if tdelta <= 0:
                 print(
-                    "You may have entered a past date, please note that the machine can only move objects into the future! Try again, please ")
+                    "You may have entered a past date, please note that the machine "
+                    "can only move objects into the future! Try again, please ")
             else:
                 return tdelta
 class time_machine():
-    def request(self, who, where, tdelta):
+    """The class that simulates actions of the time machine"""
+    def request(self, who: str, where: str, tdelta: int):
+        """The method that generates the sending message"""
         print('I am sending a ', who, ' to ', where,' ', str(tdelta), ' days forward')
     def result(self):
+        """The method that sends message about successful sending"""
         print('Sending was successful')
 
 def __main__():
     print("Pay attention! The time machine moves objects only into the future")
-    a = person()
+    a = wish()
     b = time_machine()
-    who = a.wish()
+    who = a.subject()
     where = a.place(who)
     when = a.time(who, where)
     b.request(who, where, when)
     b.result()
+
+help(wish)
